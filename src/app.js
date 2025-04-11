4ad8ff2ed3fb475062d64c9e8b5e0684e09f2a73
Nicolás Pasik <115046877+NicolasQuest@users.noreply.github.com>
2025-04-11 18:09:07 +0000

    Ejercicio completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,86 @@
-import "bootstrap";
-import "./style.css";
+
+window.onload = function () {
+
+  const suits = ["♠", "♣", "♥", "♦"];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  let cardNumber = document.querySelector("#number")
+  let symbolTop = document.querySelector("#icon")
+  let symbolBottom = document.querySelector("#secondIcon")
+  
+  
+
+  function randomCard() {
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+
+    symbolTop.innerHTML = randomSuit
+    cardNumber.innerHTML = randomValue;
+    symbolBottom.innerHTML = randomSuit
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    if (randomSuit === "♥" || randomSuit === "♦"){
+      symbolTop.style.color = "red"
+      cardNumber.style.color = "red"
+      symbolBottom.style.color = "red"
+    }
+    if (randomSuit === "♠" || randomSuit === "♣"){
+      symbolTop.style.color = "black"
+      cardNumber.style.color = "black"
+      symbolBottom.style.color = "black"
+    }
+   
+    normalValues();
+   
+  };
+  
+  let card = document.querySelector(".card")
+  let card2 = document.querySelector(".card2")
+  let height = document.querySelector("#height")
+  let width = document.querySelector("#width")
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  width.addEventListener("keyup", (e) =>{
+    
+    if (e.keyCode == 13){
+      card.style.width = `${width.value}` 
+       card2.style.display = `none`
+      
+     
+    }
+
+  })
+  height.addEventListener("keyup", (e) =>{
+    
+    if (e.keyCode == 13){
+      card.style.height = `${height.value}`
+      card2.style.display = `none`
+     
+    
+    }
+  })
+  
+  
+ 
+  //Randomizer card button
+  let randomizerButton = document.querySelector("#randomizerButton") //Tiene que ir adentro de la función?
+  randomizerButton.addEventListener("click", randomCard) //lo mismo
+
+
+ //Cambia cada 10s
+  setInterval (() => {
+    randomCard();
+  },10000);
+
+  randomCard();
+
+
+function normalValues (){
+  card.style.width = "342px";
+  card.style. height = "535px";
+  card2.style.display = "inline";
+
+}
+
+}; 
+
+
